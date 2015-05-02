7675526e73d09b8bc6a099bc9789916f25b1bb47
Ambrou <apetitgenet@free.fr>
2015-05-02 11:51:20 +0200

    Rajout du narrateur oral

--- LoupGarou/Helpers/NarrationOral.cs
+++ LoupGarou/Helpers/NarrationOral.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Speech.Synthesis;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,9 +9,17 @@ namespace JeuDuLoupGarou.Helpers
 {
     public class NarrationOral : Narration
     {
+        public SpeechSynthesizer m_Narrateur = new SpeechSynthesizer();
+
+        public NarrationOral()
+        {
+            m_Narrateur.SelectVoice("ScanSoft Virginie_Dri40_16kHz");
+            m_Narrateur.Rate = -2;
+        }
+
         public void narrer(string strHistoire)
         {
-
+            m_Narrateur.Speak(strHistoire);
         }
     }
 }
