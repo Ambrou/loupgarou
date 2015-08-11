fcf4a41cac6ba22de240f7c5902704e4d6606a22
Ambrou <apetitgenet@free.fr>
2015-08-11 18:50:40 +0100

    Rajout des fichiers manquants

---
+++ LoupGarou.Console/MaitreDuJeuConsole.cs
@@ -0,0 +1,23 @@
+﻿using LoupGarou.Core;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoupGarou.Console
+{
+    class MaitreDuJeuConsole : MaitreDuJeu
+    {
+        public void conter(string texte)
+        {
+            var color = System.Console.ForegroundColor;
+            System.Console.ForegroundColor = ConsoleColor.DarkYellow;
+            System.Console.WriteLine("Narrateur: ");
+            System.Console.ForegroundColor = color;
+            System.Console.WriteLine(texte);
+        }
+    }
+}
---
+++ LoupGarou.Core/ClassLibrary1/JeuDuLoupGarou.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LoupGarou.Core
+{
+    public class JeuDuLoupGarou
+    {
+        private Dictionary<string, string> descriptionParRole = new Dictionary<string, string>();
+        MaitreDuJeu maitreDuJeu = null;
+
+        public JeuDuLoupGarou()
+        {
+            descriptionParRole.Add(LoupGarou.Core.Properties.Resources.NomRoleVoyante,     LoupGarou.Core.Properties.Resources.DescriptionRoleVoyante);
+            descriptionParRole.Add(LoupGarou.Core.Properties.Resources.NomRoleVillageois,  LoupGarou.Core.Properties.Resources.DescriptionRoleVillageois);
+            descriptionParRole.Add(LoupGarou.Core.Properties.Resources.NomRoleCupidon,     LoupGarou.Core.Properties.Resources.DescriptionRoleCupidon);
+            descriptionParRole.Add(LoupGarou.Core.Properties.Resources.NomRoleSorciere,    LoupGarou.Core.Properties.Resources.DescriptionRoleSorciere);
+            descriptionParRole.Add(LoupGarou.Core.Properties.Resources.NomRolePetiteFille, LoupGarou.Core.Properties.Resources.DescriptionrolePetiteFille);
+            descriptionParRole.Add(LoupGarou.Core.Properties.Resources.NomRoleChasseur,    LoupGarou.Core.Properties.Resources.DescriptionRoleChasseur);
+        }
+
+        public void avecCommeMaitreDuJeu(MaitreDuJeu maitreDuJeu)
+        {
+            this.maitreDuJeu = maitreDuJeu;
+        }
+
+        public void commencerPartie()
+        {
+            maitreDuJeu.conter(LoupGarou.Core.Properties.Resources.PresentationDuJeu);
+        }
+
+        public void donneInformationRole(string nomDuRole, Joueur joueur)
+        {
+            joueur.afficheInformationRole(descriptionParRole[nomDuRole]);
+        }
+    }
+}
