f2c9ed403f0d8e7b9f5f29309b20970e2a41a524
Ambrou <apetitgenet@free.fr>
2015-08-12 18:32:46 +0100

    Rajout de la connexion des joueurs

---
+++ LoupGarou.Specs/Features/ConnexionDesJoueurs.feature.cs
@@ -0,0 +1,6 @@
+﻿#error Parsing error near 'Feature: ConnexionDesJoueurs'
+#error Parsing error near 'In order to avoid silly mistakes'
+#error Parsing error near 'As a math idiot'
+#error Parsing error near 'I want to be told the sum of two numbers'
+#error Parsing error near 'Scenario: Add two numbers'
+#error Parsing error near 'Given I have entered 50 into the calculator'
